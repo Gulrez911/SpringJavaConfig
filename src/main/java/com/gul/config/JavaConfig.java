7096deb67f5d46a363cdaab5b9e8cb3360ea3913
Gulrez <gulfarooqui1@gmail.com>
2019-05-11 11:38:37 +0530

    Java Config Spring MVC

---
+++ src/main/java/com/gul/config/JavaConfig.java
@@ -0,0 +1,21 @@
+package com.gul.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.gul")
+public class JavaConfig {
+
+	@Bean
+	public InternalResourceViewResolver resolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+}
